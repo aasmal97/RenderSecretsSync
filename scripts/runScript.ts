893089d4c86c40eebf49ed7105585b922c5c3915
Arky Asmal <arkyasmal@gmail.com>
2024-06-11 23:53:34 -0400

    add build scripts

--- scripts/runScript.ts
+++ scripts/runScript.ts
@@ -10,12 +10,12 @@ const testGeneralGithubAction = async (packageJsonPath: string) => {
     cwd: packageJsonPath,
   });
 };
-const testAllSecretsGithubAction = async (packageJsonPath: string) => {
+const testDeleteAllNotInEnv = async (packageJsonPath: string) => {
   //test github action all secrets
   const testFileAllSecretsPath = path.join(
     "test",
     "workflows",
-    "test_workflow_all_secrets.yml"
+    "test_workflow_delete_all_not_in_env.yml"
   );
   const secretsAllSecretsFilePath = path.join(
     "test",
@@ -27,30 +27,13 @@ const testAllSecretsGithubAction = async (packageJsonPath: string) => {
     cwd: packageJsonPath,
   });
 };
-const testGenerateEnvFileGithubAction = async (packageJsonPath: string) => {
-  //test github action generate env file
-  const testFileGenerateEnvFilePath = path.join(
-    "test",
-    "workflows",
-    "test_workflow_generate_env.yml"
-  );
-  const secretsGenerateEnvFilePath = path.join(
-    "test",
-    "workflows",
-    "my.secrets"
-  );
-  const generateEnvCommand = `act -W ${testFileGenerateEnvFilePath} --secret-file ${secretsGenerateEnvFilePath}`;
-  await execShellCommand(generateEnvCommand, {
-    cwd: packageJsonPath,
-  });
-};
+
 const main = async () => {
   const packageJsonPath = findPackageJson(__dirname);
   if (!packageJsonPath) return console.log("no package.json found");
   //build action.yml file
   await build();
-  await testAllSecretsGithubAction(packageJsonPath);
+  await testDeleteAllNotInEnv(packageJsonPath);
   await testGeneralGithubAction(packageJsonPath);
-  await testGenerateEnvFileGithubAction(packageJsonPath);
 };
 if (require.main === module) main();
